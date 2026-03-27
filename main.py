859a17119a2626cba6cfd2dd0cc7a61333543c3c
NiteeshGowda72 <niteeshgowda7272@gmail.com>
2026-03-27 10:05:03 +0530

    Modified code using functions

--- main.py
+++ main.py
@@ -2,8 +2,35 @@ import time
 import random
 
 with open("sentences.txt", "r") as file:
-    sentences = file.readlines()
     sentences = [line.strip() for line in file.readlines() if line.strip()]
+
+
+def compare(choice, user_input):
+    if choice.lower() == user_input.lower():
+        print("✅ Correct!")
+    else:
+        print("❌ Incorrect!")
+
+
+def calculate_wpm(user_input, time_taken):
+    words = len(user_input.split())
+    wpm = (words / time_taken) * 60 if time_taken > 0 else 0
+    print(f"Typing Speed: {round(wpm, 2)} WPM")
+    return wpm
+
+
+def calculate_accuracy(choice, user_input):
+    correct_chars = 0
+
+    for j in range(min(len(choice), len(user_input))):
+        if choice[j] == user_input[j]:
+            correct_chars += 1
+
+    total_chars = max(len(choice), len(user_input))
+    accuracy = (correct_chars / total_chars) * 100
+
+    print(f"Accuracy: {round(accuracy, 2)}%")
+    return accuracy
 
 
 def rounds(n):
@@ -11,50 +38,28 @@ def rounds(n):
 
     for round_num in range(n):
         print(f"\n--- Round {round_num + 1} ---")
+
         choice = random.choice(sentences)
 
         input("Press Enter to start...")
 
-        print("Type this:")
+        print("\nType this:")
         print(choice)
 
         start = time.time()
-
-        user_input = input("")
-
+        user_input = input("Start typing: ")
         end = time.time()
 
         time_taken = end - start
 
-        # Compare
-        if choice.strip() == user_input.strip():
-            print("✅ Correct!")
-        else:
-            print("❌ Incorrect!")
-
         print(f"⏱ Time taken: {round(time_taken, 2)} seconds")
 
-        # WPM
-        words = len(user_input.split())
-        if time_taken > 0:
-            wpm = (words / time_taken) * 60
-        else:
-            wpm = 0
+        compare(choice, user_input)
+
+        wpm = calculate_wpm(user_input, time_taken)
         scores.append(wpm)
-        print(f"Typing Speed: {round(wpm, 2)} WPM")
 
-        # Accuracy
-        correct_chars = 0
-
-        for j in range(min(len(choice), len(user_input))):
-            if choice[j] == user_input[i]:
-                correct_chars += 1
-
-        total_chars = max(len(choice), len(user_input))
-        accuracy = (correct_chars / total_chars) * 100
-        print(f"Accuracy: {round(accuracy, 2)}%")
-
-        # Scores
+        calculate_accuracy(choice, user_input)
 
     print("\n🏆 Final Results")
     print(f"Best WPM: {round(max(scores), 2)}")
